bd9c2cf3c11a91540e06b36ff9c9d74ad0c28463
Matheus <MatheusEdDominguesGanancio@Gmail.com>
2026-04-06 16:56:32 +0100

    Refactor Music to properties; add display
    Convert Music's public fields to C# auto-properties (Name, Artist, Time, Disponible), add a Description computed property, and implement ShowTechinicalsSpecifications() to print song details and availability. Update Program.cs to use the new properties, set Time and Disponible values for each track, print the Description, and call the new display method for both music instances. Also add/adjust comments and minor formatting.

--- ScreenSound/ScreenSound/Music.cs
+++ ScreenSound/ScreenSound/Music.cs
@@ -1,7 +1,24 @@
 ﻿class Music
 {
-public string name;
-public string artist;
-public int time;
-public bool disponible;
+    public string Name {  get; set; }
+    public string Artist { get; set; }
+    public int Time { get; set; }
+    public bool Disponible {  get; set; }
+    public string Description =>
+        $"The music {Name} are from {Artist}";
+
+    public void ShowTechinicalsSpecifications()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Artist: {Artist}");
+        Console.WriteLine($"Time: {Time}");
+        if (Disponible)
+        {
+            Console.WriteLine("Disponible in the plan");
+        }
+        else
+        {
+            Console.WriteLine("Want to be premium?");
+        }
+    }
 }
--- ScreenSound/ScreenSound/Program.cs
+++ ScreenSound/ScreenSound/Program.cs
@@ -1,10 +1,20 @@
-﻿Music music1 = new Music();
-music1.name = "Before i forget";
-music1.artist = "Slipknot";
+﻿// Defining technical specifications for the songs and displaying them in the console.
+//  "Music music1 = new Music()" Cria uma variavel adotando o valor
+//  (como name, artist, time e disponible da classe "Music" dentro de ScreenSound
 
-Console.WriteLine($"Music name: {music1.name}");
-Console.WriteLine($"artist name: {music1.artist}");
+Music music1 = new Music();
+music1.Name = "Before i forget";
+music1.Artist = "Slipknot";
+music1.Time = 244;
+music1.Disponible = true;
+Console.WriteLine(music1.Description);
 
 Music music2 = new Music();
-music2.name = "Boulevard of Broken Dreams";
-music2.artist = "Green Day";
+music2.Name = "Boulevard of Broken Dreams";
+music2.Artist = "Green Day";
+music2.Time = 262;
+music2.Disponible = false;
+
+
+music1.ShowTechinicalsSpecifications();
+music2.ShowTechinicalsSpecifications();
